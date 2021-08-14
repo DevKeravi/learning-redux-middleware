63f82953554a12ddd1c545bfdc26943b01ef29d8
Keravi <cmkrosp@gmail.com>
2021-08-14 18:55:51 +0900

    리덕스를 이용한 기본 카운터 제작

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
+import CounterContainer from "./containers/CounterContainer";
 function App() {
   return (
     <div className="App">
-      <h1>App</h1>
+      <CounterContainer />
     </div>
   );
 }
